46baaa6a92962265596ec10d7ccff06b7f4b550e
Daniil <traitnil@gmail.com>
2022-02-28 23:37:36 +0200

    Component

--- Component.js
+++ Component.js
@@ -3,3 +3,5 @@ class Component {
       return "Test Component";
     }
   }
+
+  exports.Component = Component;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const {MyMath} = require('./MyMath')
+const {MyMath} = require('./MyMath');
+const {Component} = require('./Component');
 
 console.log(2 + 2);
 console.log("hellow node!");
 
-console.log(MyMath);
+const c = new Component();
+c.render();
 
 const res = MyMath.sum(3, 3);
 console.log(res);

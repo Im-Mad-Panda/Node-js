a50b5672255e956207b11fe8e1dce28130bb0e47
Daniil <traitnil@gmail.com>
2022-03-01 16:49:46 +0200

    Read-n-write task resolve

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 
-fs.readFile('./text.txt', 'utf8').then((data)=>{
-    console.log(data);
+fs.readFile('./text.txt', 'utf8').then((content)=>{
+    const newContentTowrite = `Old content: ${content}, new content: Hello world`;
+    fs.writeFile('./newFile.txt', newContentTowrite, 'utf8');
 })
